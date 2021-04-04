0fdc4770db07c7964adef2d5c201896d6f3934ae
ZC <carter.zo3y@gmail.com>
2021-04-03 21:50:23 -0700

    loan calculator complete file

--- rb_101/lesson_2/loan_calculator.rb
+++ rb_101/lesson_2/loan_calculator.rb
@@ -1,23 +1,42 @@
 # methods
 def screenprint(msg)
-  puts("|| #{msg}")
+  Kernel.puts("|| #{msg}")
 end
 
 # greet
 screenprint('Hello there')
-screenprint('Let\'s find your loan stats')
+screenprint('Let\'s calculate your loan payment')
 
 # get loan amount
 screenprint('First, enter the loan amount [principal] in dollars: $')
-principal = gets.chomp.to_f
+principal = Kernel.gets().chomp().to_f()
 
 # get interest rate (APR)
-screenprint('Next, what is the interest rate [APR]?')
-
+screenprint(
+  'Next, enter the yearly interest rate [APR] (decimal or with % sign): '
+)
+apr = Kernel.gets().chomp()
 
 # get loan duration
-# set monthly interest rate
-# set loan duration months
-# set (calc) monthly payment
-# print loan stats
+screenprint('Finally, enter the length of the loan: ')
+loan_duration = Kernel.gets().chomp().to_f
 
+screenprint('Is that in (M)onths or (Y)ears?')
+loan_period = Kernel.gets().chomp().downcase()
+
+# clean APR
+apr = apr.end_with?('%') ? apr.to_f / 100.00 : apr.to_f
+
+# set monthly interest rate
+monthly_interest = apr / 12.0
+
+# set loan duration months
+loan_duration = loan_period == 'm' ? loan_duration : loan_duration * 12
+
+# set (calc) monthly payment
+payment_amt =
+  principal * (monthly_interest / (
+    1 - (1 + monthly_interest)**(-loan_duration)))
+
+# print loan stats
+screenprint("Your monthly payment will be: $#{payment_amt}")
